a462b59e0efe52df91195e78669430df24f50071
Aiden 안정희 <aiden@cntt.co.kr>
2018-01-30 18:31:43 +0900

    두 지점간 거리 계산 util 추가

---
+++ delivery-dispatch-core/src/main/java/kr/co/cntt/core/util/Misc.java
@@ -0,0 +1,38 @@
+package kr.co.cntt.core.util;
+
+/**
+ * <p> kr.co.cntt.core.util </p>
+ * <p> Misc.java </p>
+ *
+ * @author Aiden
+ * @see
+ */
+public class Misc {
+
+    /**
+     * <p> 두 지점 간 거리를 m단위로 계산
+     *
+     * @param lat1
+     * @param lon1
+     * @param lat2
+     * @param lon2
+     * @return
+     */
+    public int getHaversine(String lat1, String lon1, String lat2, String lon2) {
+        double radiansLat1 = Math.toRadians(Double.parseDouble(lat1));
+        double radiansLon1 = Math.toRadians(Double.parseDouble(lon1));
+        double radiansLat2 = Math.toRadians(Double.parseDouble(lat2));
+        double radiansLon2 = Math.toRadians(Double.parseDouble(lon2));
+
+        double dlat = radiansLat2 - radiansLat1;
+        double dlon = radiansLon2 - radiansLon1;
+
+        double a = Math.pow(Math.sin(dlat / 2), 2) + Math.cos(radiansLat1) * Math.cos(radiansLat2) * Math.pow(Math.sin(dlon / 2), 2);
+        double c = 2 * Math.asin(Math.sqrt(a));
+
+        int m = Integer.parseInt(String.valueOf(Math.round(6367 * c * 1000)));
+
+        return m;
+    }
+
+}
