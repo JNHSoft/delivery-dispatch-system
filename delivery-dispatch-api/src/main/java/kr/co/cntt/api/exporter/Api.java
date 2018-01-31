a3b38127cb6c023543fd44b3564468e4571e557a
Aiden 안정희 <aiden@cntt.co.kr>
2018-01-31 09:08:22 +0900

    api 변경

--- delivery-dispatch-api/src/main/java/kr/co/cntt/api/exporter/Api.java
+++ delivery-dispatch-api/src/main/java/kr/co/cntt/api/exporter/Api.java
@@ -28,23 +28,23 @@ public interface Api {
     // ############################################################################################ //
 
     /** Rider 정보 조회 */
-    String RIDER_INFO = "/rider/getRiderInfo.json";
+    String RIDER_INFO = "/v1/getRiderInfo.json";
 
     /** 해당 스토어 Rider 목록*/
-    String STORE_RIDERS= "/rider/getStoreRiders.json";
+    String STORE_RIDERS= "/v1/getStoreRiders.json";
 
     // ############################################################################################ //
 
     /** Store 정보 조회 */
-    String STORE_INFO = "/store/getStoreInfo.json";
+    String STORE_INFO = "/v1/getStoreInfo.json";
 
     /** Store 정보 수정  */
-    String STORE_UPDATE_INFO = "/store/updateStoreInfo.json";
+    String STORE_UPDATE_INFO = "/v1/updateStoreInfo.json";
 
     // ############################################################################################ //
 
     /** Order 등록 */
-    String ORDER_POST = "/order/postOrder.json";
+    String ORDER_POST = "/v1/postOrder.json";
 
     // ############################################################################################ //
 
