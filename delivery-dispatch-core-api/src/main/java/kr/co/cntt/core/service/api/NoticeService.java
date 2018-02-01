74076810ed9eb50243b24cc165e9c3916eddba4f
kimjisoo <kimjisoo@192.168.180.250:9000>
2018-02-01 09:22:58 +0900

    공지사항 진행 중

--- delivery-dispatch-core-api/src/main/java/kr/co/cntt/core/service/api/NoticeService.java
+++ delivery-dispatch-core-api/src/main/java/kr/co/cntt/core/service/api/NoticeService.java
@@ -1,5 +1,6 @@
 package kr.co.cntt.core.service.api;
 
+import kr.co.cntt.core.exception.AppTrException;
 import kr.co.cntt.core.model.notice.Notice;
 
 public interface NoticeService {
@@ -17,4 +18,18 @@ public interface NoticeService {
      * @return
      */
     public int updateNotice(Notice notice);
+
+    /**
+     * 공지사항 삭제
+     * @param notice
+     * @return
+     */
+    public int deleteNotice(Notice notice) throws AppTrException;
+
+    /**
+     * 공지사항 상세 보기 진행중
+     * @param notice
+     * @return
+     */
+    public Notice detailNotice(Notice notice) throws AppTrException;
 }
