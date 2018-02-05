688789eb4a16f14ad7b4ac0e207b2e9561f0c593
kimjisoo <kimjisoo@192.168.180.250:9000>
2018-02-05 16:33:16 +0900

    라이더 자기 위치 및 라이더들 위정 정보 조회

--- delivery-dispatch-core-api/src/main/java/kr/co/cntt/core/service/api/impl/RiderServiceImpl.java
+++ delivery-dispatch-core-api/src/main/java/kr/co/cntt/core/service/api/impl/RiderServiceImpl.java
@@ -5,17 +5,19 @@ import kr.co.cntt.core.exception.AppTrException;
 import kr.co.cntt.core.mapper.RiderMapper;
 import kr.co.cntt.core.model.login.User;
 import kr.co.cntt.core.model.rider.Rider;
-import kr.co.cntt.core.model.store.Store;
-import kr.co.cntt.core.model.store.StoreRiderRel;
 import kr.co.cntt.core.service.ServiceSupport;
 import kr.co.cntt.core.service.api.RiderService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @Service("riderService")
@@ -112,14 +114,78 @@ public class RiderServiceImpl extends ServiceSupport implements RiderService {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (authentication.getAuthorities().toString().equals("[ROLE_RIDER]")) {
             rider.setAccessToken(rider.getToken());
-            rider.setId("");
         } else if (authentication.getAuthorities().toString().equals("[ROLE_USER]")) {
+            // rider 가 아닌 user 가 넘어왔을때 token 값 null 로 셋팅
             rider.setAccessToken(null);
-            rider.setId("");
         }
 
         int nRet = riderMapper.updateWorkingRider(rider);
         return nRet;
 
     }
+
+    // rider 위치 정보 전송
+    @Override
+    public int updateRiderLocation(Rider rider) throws AppTrException {
+        // Role 확인 token 으로 권한 확인
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication.getAuthorities().toString().equals("[ROLE_RIDER]")) {
+            rider.setAccessToken(rider.getToken());
+        } else if (authentication.getAuthorities().toString().equals("[ROLE_USER]")) {
+            rider.setAccessToken(null);
+        }
+
+        int nRet = riderMapper.updateRiderLocation(rider);
+        return nRet;
+    }
+
+    // rider 가 자기 위치 정보 조회
+    @Override
+    public List<Rider> getRiderLocation(Rider rider) throws AppTrException {
+        // Role 확인 token 으로 권한 확인
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication.getAuthorities().toString().equals("[ROLE_RIDER]")) {
+            rider.setAccessToken(rider.getToken());
+            rider.setId("");
+        } else if (authentication.getAuthorities().toString().equals("[ROLE_USER]")) {
+            rider.setAccessToken(null);
+            rider.setId("");
+        }
+
+        List<Rider> S_Rider = riderMapper.getRiderLocation(rider);
+
+        if (S_Rider.size() == 0) {
+            throw new AppTrException(getMessage(ErrorCodeEnum.A0011), ErrorCodeEnum.A0011.name());
+        }
+        return S_Rider;
+    }
+
+    // store , admin rider 들 위치 정보 조회
+    @Secured({"ROLE_ADMIN", "ROLE_STORE"})
+    @Override
+    public Map getRidersLocation(Rider rider) throws AppTrException {
+        // list 선언
+        List<Rider> A_Rider = new ArrayList<>();
+        List<Rider> S_Rider = new ArrayList<>();
+
+        // Role 확인
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication.getAuthorities().toString().equals("[ROLE_STORE]")) {
+            // token 값 선언
+            rider.setAccessToken(rider.getToken());
+            // Store
+            S_Rider = riderMapper.getStoreRidersLocation(rider);
+        } else if (authentication.getAuthorities().toString().equals("[ROLE_ADMIN]")) {
+            rider.setAccessToken(rider.getToken());
+
+            A_Rider = riderMapper.getAdminRidersLocation(rider);
+        }
+        // map 으로 넘겨준다
+        Map<String, List<Rider>> map = new HashMap<>();
+        map.put("adminRider", A_Rider);
+        map.put("storeRider", S_Rider);
+        return map;
+    }
+
 }
