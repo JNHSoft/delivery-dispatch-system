cfa5bc7344e0d8ae3ecf2b9c24d1253e128c6825
kimjisoo <kimjisoo@192.168.180.250:9000>
2018-02-13 11:56:04 +0900

    결제 정보 등록 및 수정 및 조회

---
+++ delivery-dispatch-core/src/main/java/kr/co/cntt/core/model/payment/Payment.java
@@ -0,0 +1,28 @@
+package kr.co.cntt.core.model.payment;
+
+
+import kr.co.cntt.core.model.Dto;
+import kr.co.cntt.core.model.common.Common;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Payment extends Common implements Dto {
+
+    private static final long serialVersionUID = 8384493233610556562L;
+
+    private String adminId;
+    private String storeId;
+    private String orderId;
+    private String type;
+    private String status;
+    private String cardNumber;
+    private String approvalNumber;
+    private String approvalDate;
+    private String amount;
+    private String acquirerCode;
+    private String acquirerName;
+    private String payType;
+
+}
