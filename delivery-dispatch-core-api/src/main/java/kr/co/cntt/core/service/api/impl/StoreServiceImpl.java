91d1bf6c1fbd464355f5df385e5ddb3d938632cb
zachary <zachary@cntt.co.kr>
2018-02-28 14:43:55 +0900

    업데이트시 modified_time 적용

--- delivery-dispatch-core-api/src/main/java/kr/co/cntt/core/service/api/impl/StoreServiceImpl.java
+++ delivery-dispatch-core-api/src/main/java/kr/co/cntt/core/service/api/impl/StoreServiceImpl.java
@@ -141,7 +141,11 @@ public class StoreServiceImpl extends ServiceSupport implements StoreService {
     //배정 서드파티 설정
     @Secured({"ROLE_ADMIN", "ROLE_STORE"})
     @Override
-    public int putStoreThirdParty(Store store){return storeMapper.updateStoreThirdParty(store); }
+    public int putStoreThirdParty(Store store){
+        String[] tempStr = (store.getThirdParty()).split("(?<=\\G.{" + 1 + "})");
+        store.setThirdParty(StringUtils.arrayToDelimitedString(tempStr, "|"));
+        return storeMapper.updateStoreThirdParty(store);
+    }
 
     //배정 서드파티 목록
     @Secured({"ROLE_ADMIN", "ROLE_STORE"})
