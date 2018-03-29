df28bd62eca087e52802df2d0258423422440e64
zachary <zachary@cntt.co.kr>
2018-03-29 18:05:14 +0900

    공지사항 리스트없을때도 보이게 설정

--- delivery-dispatch-mvc/src/main/resources/js/site/setting/notice.js
+++ delivery-dispatch-mvc/src/main/resources/js/site/setting/notice.js
@@ -1,3 +1,4 @@
+/*<![CDATA[*/
 function footerRiders() {
     if(footerRiderList[2]){
         $('#rest').text(parseInt(footerRiderList[2].workCount) + parseInt(footerRiderList[2].orderCount));//휴식
@@ -40,6 +41,7 @@ function footerOrders() {
 $(function() {
     footerRiders();
     footerOrders();
+    getNoticeList()
 });
 function getNoticeList() {
     var mydata = [];
@@ -50,7 +52,6 @@ function getNoticeList() {
         dataType: 'json',
         success: function (data) {
             var i = 1;
-
             for (var key in data) {
                 if (data.hasOwnProperty(key)) {
                     var tmpdata = new Object();
@@ -58,11 +59,11 @@ function getNoticeList() {
                     tmpdata.no = i;
                     tmpdata.id = data[key].id;
 
-                    if (data[key].toGroupId != 0) {
+                    if (data[key].toGroupId) {
                         tmpdata.target = data[key].groupName;
-                        if (data[key].toSubGroupId != 0) {
+                        if (data[key].toSubGroupId) {
                             tmpdata.target = tmpdata.target + '/' + data[key].subgroupName;
-                            if (data[key].toStoreId != 0) {
+                            if (data[key].toStoreId) {
                                 tmpdata.target = tmpdata.target + '/' + data[key].storeName;
                             } else {
                                 tmpdata.target = tmpdata.target + '/' + notice_target_all;
@@ -75,7 +76,7 @@ function getNoticeList() {
                     }
 
                     tmpdata.title = data[key].title;
-                    if (data[key].fileName != null) {
+                    if (data[key].fileName) {
                         tmpdata.file = '<a href="#" class="button h20">' + notice_attach_download + '</a>'
                     } else {
                         tmpdata.file = notice_attach_none;
@@ -83,7 +84,7 @@ function getNoticeList() {
 
                     tmpdata.date = data[key].createdDatetime;
 
-                    if (data[key].confirmedDatetime != null) {
+                    if (data[key].confirmedDatetime) {
                         tmpdata.check = data[key].confirmedDatetime;
                     } else {
                         tmpdata.check = notice_confirmed_before
@@ -93,50 +94,50 @@ function getNoticeList() {
 
                     mydata.push(tmpdata);
 
-                    if (mydata != null) {
-                        jQuery('#jqGrid').jqGrid('clearGridData')
-                        jQuery('#jqGrid').jqGrid('setGridParam', {data: mydata, page: 1})
-                        jQuery('#jqGrid').trigger('reloadGrid');
-                    }
-
-                    $('#jqGrid').jqGrid({
-                        datatype: 'local',
-                        data: mydata,
-                        colModel: [
-                            {label: 'id', name: 'id', width: 25, key: true, align: 'center', hidden:true},
-                            {label: 'No', name: 'no', width: 25, key: true, align: 'center'},
-                            {label: notice_target, name: 'target', width: 60, align: 'center'},
-                            {label: notice_subject, name: 'title', width: 300},
-                            {label: notice_attach, name: 'file', width: 100, align: 'center'},
-                            {label: notice_created, name: 'date', width: 100, align: 'center'},
-                            {label: notice_confirmed, name: 'check', width: 100, align: 'center'}
-                        ],
-                        width: 'auto',
-                        height: 520,
-                        autowidth: true,
-                        rowNum: 20,
-                        pager: '#jqGridPager',
-                        onCellSelect: function (rowid, icol, cellcontent, e) {
-                            var rowData = jQuery(this).getRowData(rowid);
-                            var noticeId = rowData['id'];
-                            getNoticeDetail(noticeId);
-                            popOpen('#popNotice') //상세보기 열기
-                        }
-                    });
-
-                    resizeJqGrid('#jqGrid'); //그리드 리사이즈
-
-                    $('.store_chk .open').click(function (e) {
-                        e.preventDefault();
-                        $(this).next().slideDown();
-                    });
-
-                    $('.store_chk .close').click(function (e) {
-                        e.preventDefault();
-                        $(this).closest('div').slideUp();
-                    });
                 }
             }
+            if (mydata) {
+                jQuery('#jqGrid').jqGrid('clearGridData')
+                jQuery('#jqGrid').jqGrid('setGridParam', {data: mydata, page: 1})
+                jQuery('#jqGrid').trigger('reloadGrid');
+            }
+
+            $('#jqGrid').jqGrid({
+                datatype: 'local',
+                data: mydata,
+                colModel: [
+                    {label: 'id', name: 'id', width: 25, key: true, align: 'center', hidden:true},
+                    {label: 'No', name: 'no', width: 25, key: true, align: 'center'},
+                    {label: notice_target, name: 'target', width: 60, align: 'center'},
+                    {label: notice_subject, name: 'title', width: 300},
+                    {label: notice_attach, name: 'file', width: 100, align: 'center'},
+                    {label: notice_created, name: 'date', width: 100, align: 'center'},
+                    {label: notice_confirmed, name: 'check', width: 100, align: 'center'}
+                ],
+                width: 'auto',
+                height: 700,
+                autowidth: true,
+                rowNum: 20,
+                pager: '#jqGridPager',
+                ondblClickRow: function (rowid, icol, cellcontent, e) {
+                    var rowData = jQuery(this).getRowData(rowid);
+                    var noticeId = rowData['id'];
+                    getNoticeDetail(noticeId);
+                    popOpen('#popNotice') //상세보기 열기
+                }
+            });
+
+            resizeJqGrid('#jqGrid'); //그리드 리사이즈
+
+            $('.store_chk .open').click(function (e) {
+                e.preventDefault();
+                $(this).next().slideDown();
+            });
+
+            $('.store_chk .close').click(function (e) {
+                e.preventDefault();
+                $(this).closest('div').slideUp();
+            });
         }
     });
 }
@@ -208,3 +209,4 @@ function putNoticeConfirm() {
         }
     });
 }
+/*]]>*/
