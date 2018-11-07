035a31b8637a49561209323a15b66b128efee08b
kimjisoo <kimjisoo@192.168.180.250:9000>
2018-11-07 13:43:51 +0900

    라이더 로케이션 redis 설정

--- delivery-dispatch-core-api/src/main/java/kr/co/cntt/core/service/api/impl/RiderServiceImpl.java
+++ delivery-dispatch-core-api/src/main/java/kr/co/cntt/core/service/api/impl/RiderServiceImpl.java
@@ -197,11 +197,12 @@ public class RiderServiceImpl extends ServiceSupport implements RiderService {
 
         Rider S_Rider = riderMapper.getRiderInfo(rider);
 
+
         if (nRet != 0) {
             if (S_Rider.getSubGroupStoreRel() != null) {
-                redisService.setPublisher(Content.builder().type("rider_updated").id(S_Rider.getId()).adminId(S_Rider.getAdminId()).storeId(S_Rider.getStore().getId()).subGroupId(S_Rider.getSubGroupStoreRel().getSubGroupId()).build());
+                redisService.setPublisher(Content.builder().type("rider_location_updated").id(S_Rider.getId()).adminId(S_Rider.getAdminId()).storeId(S_Rider.getStore().getId()).subGroupId(S_Rider.getSubGroupStoreRel().getSubGroupId()).latitude(S_Rider.getLatitude()).longitude(S_Rider.getLongitude()).build());
             } else {
-                redisService.setPublisher(Content.builder().type("rider_updated").id(S_Rider.getId()).adminId(S_Rider.getAdminId()).storeId(S_Rider.getStore().getId()).build());
+                redisService.setPublisher(Content.builder().type("rider_location_updated").id(S_Rider.getId()).adminId(S_Rider.getAdminId()).storeId(S_Rider.getStore().getId()).latitude(S_Rider.getLatitude()).longitude(S_Rider.getLongitude()).build());
             }
         }
 
@@ -359,3 +360,4 @@ public class RiderServiceImpl extends ServiceSupport implements RiderService {
 
 
 }
+
