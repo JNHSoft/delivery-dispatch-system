bf816aaa10fa065264c5d045abe268cd338a553c
justin <heech@cntt.co.kr>
2020-05-12 14:16:02 +0900

    2020.05.12 1. 상태 변경 API 적용

--- delivery-dispatch-mvc/src/main/java/kr/co/deliverydispatch/controller/TrackerController.java
+++ delivery-dispatch-mvc/src/main/java/kr/co/deliverydispatch/controller/TrackerController.java
@@ -1,6 +1,7 @@
 package kr.co.deliverydispatch.controller;
 
 import com.google.gson.Gson;
+import com.mysql.cj.util.StringUtils;
 import kr.co.cntt.core.annotation.CnttMethodDescription;
 import kr.co.cntt.core.model.tracker.Tracker;
 import kr.co.cntt.core.util.AES256Util;
@@ -104,16 +105,6 @@ public class TrackerController {
                 System.out.println("decAes1 = " + decAes1);
                 System.out.println("#####################################################");
             }
-
-//            if (!StringUtils.isNullOrEmpty(aseValue)){
-//                String decBase1 = CustomEncryptUtil.decodeBase64(aseValue);
-//                String decAes1 = aesUtil.aesDecode(CustomEncryptUtil.decodeBase64(aseValue));
-//
-//                System.out.println("#####################################################");
-//                System.out.println("decBase1 = " + decBase1);
-//                System.out.println("decAes1 = " + decAes1);
-//                System.out.println("#####################################################");
-//            }
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         } catch (NoSuchAlgorithmException e) {
