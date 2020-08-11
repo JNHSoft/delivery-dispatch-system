db31883efa2877552ff20ef86789ac978f3fd7e3
justin <heech@cntt.co.kr>
2020-08-11 13:20:43 +0900

    2020.08.11 1. 라이더 프로세스 변경 작업 진행 중

--- delivery-dispatch-mvc/src/main/resources/js/site/rider/rider_approval.js
+++ delivery-dispatch-mvc/src/main/resources/js/site/rider/rider_approval.js
@@ -95,31 +95,30 @@ function makeRowButton(obj){
     var btn_setDate = "";
     var btn_edit = "";
 
-    alert(obj.approvalStatus);
-
     switch (obj.approvalStatus){
         case "0":           // 요청
-            btn_approval = "<button class='button btn_pale_green h30 w110 mr10' style='font-size: 14px;' onclick='javascript:riderApproval(" + obj.id + ")'> Approval</button>";
+            btn_approval = "<button class='button btn_pale_green h30 w110 mr10' style='font-size: 14px;' onclick='javascript:riderApprovalStatus(" + obj.id + ")'> Approval</button>";
             btn_setDate = "<button class='button btn_pink h30 w180 mr10' style='font-size: 14px;'>Validity period Setting</button>"
-            btn_edit = "<button class='button btn_blue h30 w80' style='font-size: 14px'>Edit</button>"
+            btn_edit = "<button class='button btn_blue h30 w80' style='font-size: 14px' onclick='javascript:searchRiderApprovalDetail(" + obj.id + ")'>Edit</button>"
             break;
         case "1":           // 수락
             btn_approval = "<button class='button btn_onahau h30 w110 mr10' style='font-size: 12px;'><i class='fa fa-check mr5' />Approval</button>";
             btn_setDate = "<button class='button btn_weppep h30 w180 mr10' style='font-size: 12px;'><i class='fa fa-check mr5 t_pink' />Validity period Setting</button>"
-            btn_edit = "<button class='button btn_blue h30 w80' style='font-size: 14px;'>Edit</button>"
+            btn_edit = "<button class='button btn_blue h30 w80' style='font-size: 14px;' onclick='javascript:searchRiderApprovalDetail(" + obj.id + ")'>Edit</button>"
             break;
         case "2":           // 거절
         case "3":           // 승인 후 거절
-            btn_approval = "<button class='button btn_gray2 h30 w110 mr10' style='font-size: 12px;'><i class='fa fa-check mr5' />Disapproval</button>";
-            btn_setDate = "<button class='button btn_gray2 h30 w180 mr10' style='font-size: 12px;'><i class='fa fa-check mr5' />Validity period Setting</button>"
-            btn_edit = "<button class='button btn_blue h30 w80' style='font-size: 14px'>Edit</button>"
+            btn_approval = "<button class='button btn_gray2 h30 w110 mr10' style='font-size: 12px;' disabled><i class='fa fa-check mr5' />Disapproval</button>";
+            btn_setDate = "<button class='button btn_gray2 h30 w180 mr10' style='font-size: 12px;' disabled><i class='fa fa-check mr5' />Validity period Setting</button>"
+            btn_edit = "<button class='button btn_blue h30 w80' style='font-size: 14px' disabled>Edit</button>"
             break;
     }
 
     return  btn_approval + btn_setDate + btn_edit;
 }
 
-function riderApproval(rowID){
+// 상태 변경
+function riderApprovalStatus(rowID){
     //changeApprovalStatus
     loading.show();
     $.ajax({
@@ -142,6 +141,33 @@ function riderApproval(rowID){
         }
     });
 
+}
+
+// 라이더 승인 상세 조회
+function searchRiderApprovalDetail(rowID){
+    console.log(rowID);
+
+    $.ajax({
+        url: "/getRiderApprovalInfo",
+        type: "post",
+        data:{
+            id: rowID
+        },
+        dataType: "json",
+        success: function (data){
+            console.log("success : ");
+            console.log(data);
+
+            popOpen("#popRiderInfo");
+        },
+        error: function (error){
+            console.log(error);
+        },
+        complete: function (data){
+            console.log("complete");
+            console.log(data);
+        }
+    });
 }
 
 /*]]>*/
