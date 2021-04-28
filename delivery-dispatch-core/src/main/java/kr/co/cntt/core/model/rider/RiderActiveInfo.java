3b49ac947f3d62124d09de5a94af37bfeb3c90df
justin <dlrmf3390@naver.com>
2021-04-28 14:47:48 +0900

    21.04.28
    1. 라이더 활동량의 시간 형식 변경
       -> mm:ss
    
    2. D7, D30 비율 소수점 1자리로 변경

--- delivery-dispatch-core/src/main/java/kr/co/cntt/core/model/rider/RiderActiveInfo.java
+++ delivery-dispatch-core/src/main/java/kr/co/cntt/core/model/rider/RiderActiveInfo.java
@@ -10,7 +10,7 @@ import lombok.Setter;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class RiderActiveInfo  implements Dto {
     private Integer orderCount;         // 라이더가 처리한 전체 주문 수
-    private Integer avgD7Times;         // 라이더의 D7 평균
+    private String avgD7Times;         // 라이더의 D7 평균
     private String avgD7;               // 라이더의 D7 비율
     private Integer avgD30Times;        // 라이더의 D30 평균
     private String avgD30;              // 라이더의 D30 비율
