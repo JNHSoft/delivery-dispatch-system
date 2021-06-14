bdbe26c147b84fb8a3236ae78664283ea0aae806
justin <dlrmf3390@naver.com>
2021-06-14 17:12:21 +0900

    2021.06.14
    1. 라이더 공유 순서 역순으로 표기 될 수 있도록 적용

--- delivery-dispatch-core-api/src/main/java/kr/co/cntt/core/service/api/impl/OrderServiceImpl.java
+++ delivery-dispatch-core-api/src/main/java/kr/co/cntt/core/service/api/impl/OrderServiceImpl.java
@@ -225,7 +225,7 @@ public class OrderServiceImpl extends ServiceSupport implements OrderService {
                                 return true;
                         }
                     })                  // 주문 상태 값이 픽업 또는 배달 중인 경우 삭제
-                    .sorted(Comparator.comparing(Rider::getSharedStatus, Comparator.nullsLast(Comparator.naturalOrder()))       // 1순위 쉐어 내용에 따른 정렬
+                    .sorted(Comparator.comparing(Rider::getSharedStatus, Comparator.nullsLast(Comparator.reverseOrder()))       // 1순위 쉐어 내용에 따른 정렬
                             .thenComparing(Rider::getMinOrderStatus, Comparator.nullsFirst(Comparator.naturalOrder()))          // 2순위 상태값 정렬 (현재는 배정과 미배정만 나온다)
                             .thenComparing(Rider::getDistance)                                                                  // 3순위 거리 순
                             .thenComparing(Rider::getAssignCount)                                                               // 4순위 주문을 가지고 있는 순서
