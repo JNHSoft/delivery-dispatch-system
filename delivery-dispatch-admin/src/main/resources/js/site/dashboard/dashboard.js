0a630e83a5f8df782b64230d2fd89d381f7f5bf2
justin <dlrmf3390@naver.com>
2021-06-17 18:55:13 +0900

    2021.06.17
    1. 대쉬 보드 추가 중

--- delivery-dispatch-admin/src/main/resources/js/site/dashboard/dashboard.js
+++ delivery-dispatch-admin/src/main/resources/js/site/dashboard/dashboard.js
@@ -57,13 +57,79 @@ function makeEventBind(){
             return;
         }
 
+        // 오늘일자, 시작일자, 종료일자를 변수에 담아 놓는다.
+        let today = new Date($.datepicker.formatDate('yy-mm-dd', new Date));
+        let startDate = new Date($("#startDate").val());
+        let endDate = new Date($("#endDate").val());
+
         // 현재 클래스 중 on 항목에 대한 내용 제거
         $(".search-date").removeClass('on');
         // 현재 클릭 이벤트가 발생된 곳에 on Class 추가
         $(this).addClass('on');
 
+        console.log("value = > ", $(this).val());
+
         // 클릭된 항목에 따라, 이벤트가 진행되도록 함.
-        console.log($(this).val());
+        switch ($(this).val()){
+            case "today":
+                // 조회 시작일과 종요일을 오늘 날짜로 세팅한다.
+                $("#startDate, #endDate").val($.datepicker.formatDate('yy-mm-dd', today));
+                break;
+            case "lseven":
+                // 1. 시작일로 부터 당일까지 7일 미만인 경우, 종료일을 기준으로 7일 전 데이터로 보여준다.
+                if (today.getTime() - startDate.getTime() < 6 * 24 * 3600 * 1000){
+                    let beforeSeven = endDate;
+
+                    beforeSeven.setDate(endDate.getDate() - 6);
+
+                    $("#startDate").val($.datepicker.formatDate('yy-mm-dd', beforeSeven));
+                    console.log("No1.");
+                }
+                // 2. 시작일 ~ 종료일 차이가 7일 차이가 아니라면, 시작일 ~ 당일까지의 차이가 7일 초과인 경우에는 시작일을 기준으로 7일을 보여준다.
+                else if (((startDate.getTime() - endDate.getTime() < 6 * 24 * 3600 * 1000) || (startDate.getTime() - endDate.getTime() > 6 * 24 * 3600 * 1000))){
+                    let beforeSeven;
+                    let changeObj;
+
+                    if ( (today.getTime() - startDate.getTime() < 6 * 24 * 3600 * 1000)){
+                        // 시작일 ~ 오늘까지의 차이가 7일 이상인 경우, 종료일을 기준으로 계산 # 변화는 StartDate가
+                        beforeSeven = endDate;
+                        changeObj = $("startDate");
+
+                        beforeSeven.setDate(endDate.getDate() - 6);
+                        console.log("No2.");
+                    }else {
+                        // 위 조건 이외에는 종료일자 기준으로 7일 계산 # 변화는 EndDate가
+                        beforeSeven = startDate;
+                        changeObj = $("#endDate");
+
+                        beforeSeven.setDate(startDate.getDate() + 6);
+                        console.log("No3.");
+                    }
+
+                    changeObj.val($.datepicker.formatDate('yy-mm-dd', beforeSeven));
+                }
+                // 위 2가지 조건이 모두 불충분할 때
+                else{
+                    let beforeSeven = endDate;
+
+                    beforeSeven.setDate(endDate.getDate() - 6);
+
+                    $("#startDate").val($.datepicker.formatDate('yy-mm-dd', beforeSeven));
+                    console.log("No4.");
+                }
+
+                break;
+            case "cmonth":          // 이번달의 데이터이므로, today 기준의 달로 변경한다.
+                let monthFirstDay = new Date(today.getFullYear(), today.getMonth(), 1);
+
+                $("#endDate").val($.datepicker.formatDate('yy-mm-dd', today));
+                $("#startDate").val($.datepicker.formatDate('yy-mm-dd', monthFirstDay));
+
+                break;
+        }
+
+        // 날짜 지정이 완료된 후 데이터 갱신
+        searchList();
     });
     
     // 달력의 시작일자에 대한 이벤트
@@ -72,16 +138,31 @@ function makeEventBind(){
         let startDate = $(this).val();
         let endDate = $("#endDate").val();
 
+        console.log("start Date 변경 이벤트 발생");
+
         // 오늘보다 큰 경우, 오늘 일자로 강제 변경
         if (today < startDate){
             $(this).val(today);
             $("#endDate").val(today);
-            return;
+        }
+        // 시작일이 종료일보다 큰 경우 종료일을 시작일로 변경
+        else if (startDate > endDate){
+            $("#endDate").val(startDate);
         }
 
-        // 시작일이 종료일보다 큰 경우 종료일을 시작일로 변경
-        if (startDate > endDate){
-            $("#endDate").val(startDate);
+        // 날짜 범위를 체크하여 today, last 7 days, current Month 를 초기화 하거나 선택적으로 on을 시킨다.
+        $(".search-date").removeClass('on');
+        let nowDate = new Date(today);
+        let sDate = new Date($("#startDate").val());
+        let eDate = new Date($("#endDate").val());
+        let cMonthFirstDay = new Date(nowDate.getFullYear(), nowDate.getMonth(), 1, 9);
+
+        if (sDate.getTime() === eDate.getTime() && sDate.getTime() === nowDate.getTime()){
+            $("#btnToday").addClass('on');
+        }else if (eDate.getTime() - sDate.getTime() === 6 * 24 * 3600 * 1000){
+            $("#btnLastSeven").addClass('on');
+        }else if (sDate.getTime() === cMonthFirstDay.getTime() && eDate.getTime() === nowDate.getTime()){
+            $("#btnCurrentMonth").addClass('on');
         }
 
         searchList();
@@ -93,16 +174,31 @@ function makeEventBind(){
         let startDate = $("#startDate").val();
         let endDate = $(this).val();
 
+        console.log("end Date 변경 이벤트 발생");
+
         // 오늘보다 큰 경우, 오늘 일자로 강제 변경
         if (today < endDate){
             $(this).val(today);
-            $("#startDate").val(today);
-            return;
+            //$("#startDate").val(today);
+        }
+        // 종료일이 시작일보다 작은 경우 시작일을 종료일 기간으로 맞춘다.
+        else if (startDate > endDate){
+            $("#startDate").val(endDate);
         }
 
-        // 종료일이 시작일보다 작은 경우 시작일을 종료일 기간으로 맞춘다.
-        if (startDate > endDate){
-            $("#startDate").val(endDate);
+        // 날짜 범위를 체크하여 today, last 7 days, current Month 를 초기화 하거나 선택적으로 on을 시킨다.
+        $(".search-date").removeClass('on');
+        let nowDate = new Date(today);
+        let sDate = new Date($("#startDate").val());
+        let eDate = new Date($("#endDate").val());
+        let cMonthFirstDay = new Date(nowDate.getFullYear(), nowDate.getMonth(), 1, 9);
+
+        if (sDate.getTime() === eDate.getTime() && sDate.getTime() === nowDate.getTime()){
+            $("#btnToday").addClass('on');
+        }else if (eDate.getTime() - sDate.getTime() === 6 * 24 * 3600 * 1000){
+            $("#btnLastSeven").addClass('on');
+        }else if (sDate.getTime() === cMonthFirstDay.getTime() && eDate.getTime() === nowDate.getTime()){
+            $("#btnCurrentMonth").addClass('on');
         }
 
         searchList();
