7d9c30a66498afffcac6f69a83d425bd9644cacc
justin <heech@jnhsoft.com>
2021-09-23 22:01:17 +0900

    2021-09-23
    1. 배정 시간을 정확히 QT 타임 전으로 변경한다.

--- delivery-dispatch-admin/src/main/java/kr/co/cntt/deliverydispatchadmin/controller/StatisticsController.java
+++ delivery-dispatch-admin/src/main/java/kr/co/cntt/deliverydispatchadmin/controller/StatisticsController.java
@@ -371,8 +371,9 @@ public class StatisticsController {
                 // 배정 시간이 예약 시간 - QT 시간보다 늦어진 경우에 예약 - QT 시간으로 계산한다.
                 LocalDateTime assignTime = LocalDateTime.parse((a.getAssignedDatetime()).replace(" ", "T"));
                 LocalDateTime qtAssignTime = reserveDatetime.minusMinutes(qtTime);
-
-                if (ChronoUnit.MINUTES.between(assignTime, qtAssignTime) < 0){
+                
+                // 초 단위로 비교하여, 예약 시간 - QT가 될 수 있도록 적용한다
+                if (ChronoUnit.SECONDS.between(assignTime, qtAssignTime) < 0){
                     assignTime = qtAssignTime;
                     a.setAssignedDatetime(assignTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.S")));
                 }
@@ -444,7 +445,8 @@ public class StatisticsController {
                 LocalDateTime assignTime = LocalDateTime.parse((a.getAssignedDatetime()).replace(" ", "T"));
                 LocalDateTime qtAssignTime = reserveDatetime.minusMinutes(qtTime);
 
-                if (ChronoUnit.MINUTES.between(assignTime, qtAssignTime) < 0){
+                // 초 단위로 비교하여, 예약 시간 - QT가 될 수 있도록 적용한다
+                if (ChronoUnit.SECONDS.between(assignTime, qtAssignTime) < 0){
                     assignTime = qtAssignTime;
                     a.setAssignedDatetime(assignTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.S")));
                 }
